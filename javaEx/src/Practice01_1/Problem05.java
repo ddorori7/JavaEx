2dccc45abe70182243a1e230ba3385f65542d705
서지원 <qrd7079@gmail.com>
2021-07-21 11:26:14 +0900

    String 참조 타입

--- javaEx/src/Practice01_1/Problem05.java
+++ javaEx/src/Practice01_1/Problem05.java
@@ -20,18 +20,15 @@ public class Problem05 {
 		int num5 = scanner.nextInt();
 		
 		int max = num1;
-		if (num2 > max) {
+		if (num2 > max) 
 			max = num2;
-			if (num3 > max) {
-				max = num3;
-				if (num4 > max) {
-					max = num4;
-					if (num5 > max) {
-						max = num5;
-					}
-				}
-			}
-		}
+		if (num3 > max) 
+			max = num3;
+		if (num4 > max) 
+			max = num4;
+		if (num5 > max) 
+			max = num5;
+					
 		scanner.close();
 		System.out.println("최대값은 " + max + "입니다.");
 		
